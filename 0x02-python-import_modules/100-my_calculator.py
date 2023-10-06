bbf32e7f95c205501e1e1e75a68bc463b9f27d99
Lerato kgodumo <kgodumo345@gmail.com>
2023-10-05 22:32:41 -0700

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,3 +2,9 @@
 if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     import sys
+    args = sys.argv
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if args[2] not in "+-*/":
