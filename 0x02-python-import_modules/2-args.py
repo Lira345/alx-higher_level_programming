400663871a4ccb2e110f0c21d7b66740cd45b7eb
Lerato kgodumo <kgodumo345@gmail.com>
2023-10-05 22:47:10 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 2:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
-    else:
-        print("{:d} argument:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+    i = len(sys.argv) - 1
+
+    if i == 0:
 
