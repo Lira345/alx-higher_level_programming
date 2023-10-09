c685510edc2461d2971c89fab23d2edd572a9075
Lerato kgodumo <kgodumo345@gmail.com>
2023-10-09 00:39:05 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure tuple_a has at least 2 elements, filling with zeros if necessary
     tuple_a += (0, 0)
-    # Ensure tuple_b has at least 2 elements, filling with zeros if necessary
     tuple_b += (0, 0)
-    
-    # Sum the first two elements of each tuple and return as a new tuple
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+return (len(tuple_a) < 2:
+~                  
