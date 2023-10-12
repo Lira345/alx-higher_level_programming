8582c455e4c367e39c90d90bc9e3ddefcc826274
Lerato kgodumo <kgodumo345@gmail.com>
2023-10-11 23:40:10 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if item == search else item for item in my_list]
-
-my_list = [1, 2, 3, 2, 4, 5]
-print(search_replace(my_list, 2, 8))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
