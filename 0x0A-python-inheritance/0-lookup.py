0f6cf43805b6ca52566cd857ccb6d8129c1edbf0
Lerato kgodumo <kgodumo345@gmail.com>
2023-11-06 02:36:32 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
+"""  Returns the list of available attributes
+and methods of an object
 """
-    0-lookup: lookup()
-"""
-
 
 
 def lookup(obj):
-    """
-        Returns the list of available attributes and methods.
-        Args:
-            obj (object): object.
-    """
-    return (dir(obj))
+    """Returns the list of available attributes"""
+    return dir(obj)
