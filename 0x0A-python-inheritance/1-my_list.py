23e5b28a42cf6864e2a854baea542208ab742a0b
Lerato kgodumo <kgodumo345@gmail.com>
2023-11-06 00:23:08 -0800

    tasks for phython inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class MyList(list):
+    """
+        This class inherits from list.
+        Attributes:
+        Methods:
+            print_sorted - prints the list in ascending order
+    """
+    def print_sorted(self):
+        """
+           prints a list in ascending order.
+        """
+        print(sorted(self))
