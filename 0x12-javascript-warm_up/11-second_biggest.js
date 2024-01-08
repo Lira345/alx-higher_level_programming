2110680f596a32a3af74811121e703b0d40e39a3
Lerato kgodumo <kgodumo345@gmail.com>
2024-01-08 02:11:20 -0800

    task 11 update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-
-const { argv } = require('process');
-const args = argv.slice(2);
-let result = 0;
-let finalArray = [];
-
-if (args.length > 1) {
-  finalArray = [...new Set(args.map((e) => parseInt(e)).sort((a, b) => b - a))];
-  result = finalArray.length > 1 ? finalArray[1] : finalArray[0];
+if (!process.argv[2] || !process.argv[3]) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2);
+  const intArgs = args.sort((a, b) => a - b);
+  console.log(intArgs[intArgs.length - 2]);
 }
-
-console.log(result);
